a432b9847662feb086a69bbd7640f1d52b23d228
abdulhafeezarain <abdulhafeezarain060@gmail.com>
2026-03-06 10:40:01 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Foodies Pakistan Website Loaded");
+
+
+    const menuToggle = document.querySelector(".menu-toggle");
+    const nav = document.querySelector("nav");
+
+    if(menuToggle){
+        menuToggle.addEventListener("click", function(){
+            nav.classList.toggle("active");
+        });
+    }
 });
 
 function orderFood(item) {
     alert("Your order for " + item + " has been placed successfully!");
 }
-
-// Search Menu
-// function searchFood() {
-//     let input = document.getElementById("search").value.toLowerCase();
-//     let items = document.querySelectorAll(".food-item");
-
-//     items.forEach(function (item) {
-//         let name = item.innerText.toLowerCase();
-
-//         if (name.includes(input)) {
-//             item.style.display = "block";
-//         } else {
-//             item.style.display = "none";
-//         }
-//     });
-// }
 
 function sendMessage() {
     alert("Your message has been sent successfully!");
